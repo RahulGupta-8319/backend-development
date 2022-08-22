25ff5ebc40db05abecff963724c06851d7ed437e
rahul <guptagi5050@gmail.com>
2022-08-22 12:33:11 +0530

    connect one collection to another coll

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,15 +1,33 @@
-const AuthorModel= require("../models/authorModel")
+// const AuthorModel= require("../models/authorModel")
 
-const createAuthor= async function (req, res) {
+// const createAuthor= async function (req, res) {
+//     let author = req.body
+//     let authorCreated = await AuthorModel.create(author)
+//     res.send({data: authorCreated})
+// }
+
+// const getAuthorsData= async function (req, res) {
+//     let authors = await AuthorModel.find()
+//     res.send({data: authors})
+// }
+
+// module.exports.createAuthor= createAuthor
+// module.exports.getAuthorsData= getAuthorsData
+
+//----------------------------------------------------------//
+
+const AuthorModel = require("../models/authorModel")
+
+const createAuthor = async function (req, res){
     let author = req.body
     let authorCreated = await AuthorModel.create(author)
     res.send({data: authorCreated})
 }
 
-const getAuthorsData= async function (req, res) {
-    let authors = await AuthorModel.find()
+const getAuthorsData = async function(req, res){
+    let authors = await AuthorModel.find()//.populate('author publisher')
     res.send({data: authors})
 }
 
-module.exports.createAuthor= createAuthor
-module.exports.getAuthorsData= getAuthorsData
+module.exports.createAuthor = createAuthor
+module.exports.getAuthorsData = getAuthorsData
--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,23 +1,87 @@
 const authorModel = require("../models/authorModel")
-const bookModel= require("../models/bookModel")
+const bookModel = require("../models/bookModel")
+const publisherModel = require("../models/publisherModel")
 
-const createBook= async function (req, res) {
-    let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
+const createBook = async function (req, res) {
+    // let book = req.body
+    // let bookCreated = await bookModel.create(book)
+    // res.send({data: bookCreated})
+
+    //-------------------------------------------------------------//
+
+    let authorId = req.body.author
+    let publisherId = req.body.publisher
+
+    if (!authorId) {
+
+        return res.send({ msg: "write author id" })
+    }
+
+    let present = await authorModel.findById(authorId)
+    if (present) {
+        if (!publisherId) {
+            return res.send({ msg: "plz provide publisherId" })
+        }
+        let publisher = await publisherModel.findById(publisherId)
+        if (publisher) {
+
+            let book = req.body
+            let bookCreated = await bookModel.create(book)
+            res.send({ data: bookCreated })
+
+        } else {
+            return res.send({ msg: "publisher id is not present" })
+        }
+
+    }
+    else {
+        return res.send({ msg: "author id is not present " })
+    }
+
 }
 
-const getBooksData= async function (req, res) {
+const getBooksData = async function (req, res) {
     let books = await bookModel.find()
-    res.send({data: books})
+    res.send({ data: books })
 }
 
 const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
-    res.send({data: specificBook})
+    let specificBook = await bookModel.find().populate('author publisher')
+    res.send({ data: specificBook })
 
 }
 
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+// const updateBookCover = async function (req, res) {
+//     let cover = await bookModel.updateMany({ $or: [{ publisher: "Penguine" }, { publisher: "HarperCollins" }] }, { $set: { isHardCover: false } }, { new: true })
+//     res.send({ data: cover })
+// }
+
+// const increaseValue = async function (req, res) {
+//     let updatedValue = await bookModel.updateMany({ ratings: { $gt: 3 } }, { $inc: { prices: 10 } }, { new: true })
+//     res.send({ data: updatedValue })
+// }
+
+//----------------------------------------------------------------------------------------------------------------------------//
+
+const updateBookCover = async function(req, res){
+        let cover = await  publisherModel.find({ $or : [{ name : "penguine"}, { name : "HarperCollins"}]}).select({ _id : 1})
+        let update = await bookModel.updateMany({ publisher:cover}, { $set : { isHardCover: true  }})
+        res.send({data:update})
+
+}
+
+// b) For the books written by authors having a rating greater than 3.5,
+//  update the books price by 10 (For eg if old price for such a book is 50, new will be 60) 
+
+const increaseValue = async function( req, res){
+    let author1 = await authorModel.find({ rating : {$gt : 3.5}}).select({_id:1})
+    let update = await bookModel.updateMany({ author : author1 }, { $inc : { price : 10 }})
+    res.send({data : update })
+
+}
+
+module.exports.createBook = createBook
+module.exports.getBooksData = getBooksData
 module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.updateBookCover = updateBookCover
+module.exports.increaseValue = increaseValue   
